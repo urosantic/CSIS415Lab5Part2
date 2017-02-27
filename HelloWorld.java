93caa65f6c12df6f22e8ea29accfffc6beb2c995
urosantic <u18anti@siena.edu>
2017-02-27 18:21:32 -0500

    Revert "Added a comment"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,12 @@
 package Shapes;
 
 
-//No comment :(
+/**
+ * Write a description of class test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class HelloWorld
 {
     public static void main(String[] args)  {
